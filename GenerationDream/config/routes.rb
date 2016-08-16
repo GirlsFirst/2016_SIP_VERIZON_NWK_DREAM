90c21757333d6fc292d6e5a68dc6952a8b7850d1
Michelle Bautista <aermichelle@gmail.com>
2016-08-16 11:35:10 -0400

    media page video

--- GenerationDream/config/routes.rb
+++ GenerationDream/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #get 'pages/home'
   root 'pages#home'
   get 'about' => 'pages#about'
-
+  get 'scholar'=>'pages#scholar'
+  get 'benefits'=> 'pages#benefits'
+  get 'media' => 'pages#media'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
